17986e203e42a256026467cd3b01ef77737bbbdb
Pawel Sikora <pawel@purpledecks.com>
2025-12-18 17:11:12 +0100

    Initial Commit

---
+++ CloverFinder/CloverFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloverFinderApp.swift
+//  CloverFinder
+//
+//  Created by Pawel Sikora on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CloverFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
